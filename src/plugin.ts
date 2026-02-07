615c98dab4516366303454424199cd8481c79bba
AuraWave Dev <dev@aurawave.com>
2026-02-07 10:51:15 +0800

    Change file name to txgain

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,6 +1,6 @@
 import streamDeck from "@elgato/streamdeck";
 
-import { IncrementCounter } from "./actions/increment-counter";
+import { IncrementCounter } from "./actions/txgain";
 
 // We can enable "trace" logging so that all messages between the Stream Deck, and the plugin are recorded. When storing sensitive information
 streamDeck.logger.setLevel("trace");
